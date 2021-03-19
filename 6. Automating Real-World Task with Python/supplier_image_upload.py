82a55619409efcd3dce1c971aa0a0f786dc1d959
indahpuspita17 <indah_indahpuspita@apps.ipb.ac.id>
2021-03-19 23:47:22 +0700

    Update Week-4

---
+++ 6. Automating Real-World Task with Python/supplier_image_upload.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+import os, sys
+import requests
+
+url = "http://localhost/upload/"
+path = "supplier-data/images/"
+
+images = os.listdir(path)
+
+for image in images:
+  if image.endswith(".jpeg"):
+    with open(path + image, 'rb') as opened:
+      r = requests.post(url, files={'file': opened})
+© 2021 GitHub, Inc.
